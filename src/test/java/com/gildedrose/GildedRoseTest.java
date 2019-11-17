6f9c084f263cec841a9171ac41e3d5fcd3ef8e86
Łukasz Piskor <lukasz1996@outlook.com>
2019-11-17 01:47:51 +0100

    Test change

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,26 @@ package com.gildedrose;
 
 import static org.junit.Assert.*;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void updateQuality() throws Exception{
+        CombinationApprovals.verifyAllCombinations(
+                this::doUpdate,
+                new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", "Conjured Mana Cake"},
+                new Integer[]{-1, 0, 2, 6, 11},
+                new Integer[]{0, 1, 49, 50});
+    }
+
+    private String doUpdate(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        return app.items[0].toString();
     }
 
 }
